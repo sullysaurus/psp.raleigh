b2fbbc24ab1a4d8e9e07695a80d52caf80a53db3
Danny Sullivan <daniel-sullivan@pluralsight.com>
2023-10-11 17:18:20 -0400

    initial

---
+++ src/config/footer.js
@@ -0,0 +1,51 @@
+export const footerSocials = [
+  {
+    name: 'Instagram',
+    url: 'https://instagram.com/psp.raleigh',
+    icon: "mdi:instagram",
+  },
+  {
+    name: 'Youtube',
+    url: 'https://www.youtube.com/@psp.raleigh',
+    icon: "mdi:youtube",
+  },
+  {
+    name: 'Facebook',
+    url: 'https://facebook.com/psp.raleigh',
+    icon: "mdi:facebook",
+  }
+]
+
+
+export const footerLists = [
+	{
+		title: 'Learn',
+		items: [
+			{
+				title: 'Home',
+				slug: '/',
+			},
+			{
+				title: 'Blog',
+				slug: '/blog',
+			},
+            {
+				title: 'About',
+				slug: '/about',
+			}
+		],
+	},
+	{
+		title: 'Company',
+		items: [
+			{
+				title: 'Contact',
+				slug: '/contact',
+			},
+			{
+				title: 'Quote',
+				slug: '/quote',
+			},
+		],
+	}
+];
---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Popstar Productions | Photo booth rental in Raleigh, NC`,
+	description: `A modern open air photo booth rental company in Raleigh, NC`,
+	url: `https:/popstarproductions`, // No trailing slash!
+	name: `Popstar Productions`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
