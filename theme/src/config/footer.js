7670af44f99985ce88f5350cedf1d591ef42497b
Danny Sullivan <daniel-sullivan@pluralsight.com>
2023-11-30 09:15:38 -0500

    date footer

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -44,7 +44,7 @@ export const footerLists = [
 			},
 			{
 				title: 'Check Date',
-				slug: '/date',
+				slug: 'https://popstar-photo-booths.checkcherry.com/availability_calendar?props=%7B%22showBookNowButton%22%3Atrue%2C%22bookNowButtonText%22%3A%22Book%20Now%22%2C%22showCustomBookingUrl%22%3Afalse%2C%22customBookingUrl%22%3A%22%22%2C%22availableMessage%22%3A%22Congratulations%2C%20we%20are%20currently%20available%20for%20this%20day%20and%20package.%20%20Just%20click%20%26%2339%3BBook%20Now%26%2339%3B%20to%20reserve%20your%20date%20now.%22%2C%22unavailableMessage%22%3A%22Sorry%2C%20but%20we%20are%20not%20able%20to%20take%20automated%20bookings%20for%20this%20date%20or%20package.%20%20Please%20contact%20us%20directly%20to%20check%20for%20available%20options.%22%2C%22theme%22%3A%22normal%22%7D',
 			},
 		],
 	}
